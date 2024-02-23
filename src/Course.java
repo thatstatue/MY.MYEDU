d2005fd6ac2f5323ab9e19df34430aa2b3064eee
NoteBook <saradoublemohammadi@gmail.com>
2024-02-23 15:48:03 +0330

    course display method initiated.

--- src/Course.java
+++ src/Course.java
@@ -2,19 +2,33 @@ import java.util.ArrayList;
 
 public abstract class Course {
     private final String name;
+    private final School school;
     private final String teacher;
     private final int code;
     private final int units;
-    private final String days;
-    private final int hours;
+    private final int[] days;
+    private final int[] hours;
     private final int group;
     private ArrayList<Student> regStudents;
 
+    private final String[] dayNumbers = new String[]{
+            "sat", "sun","mon","tue","wed","thu"
+    };
+    private final String[] startHours = new String[]{
+            "07:30", "08:00", "08:30", "09:00",
+            "09:30", "10:00", "10:30", "11:00",
+            "11:30", "12:00", "12:30", "13:00",
+            "13:30", "14:00", "14:30", "15:00",
+            "15:30", "16:00", "16:30", "17:00",
+            "17:30", "18:00", "18:30", "19:00",
+            "19:30", "20:00", "20:30"
+    };
+
+
     private int capacity;
 
-    public Course(String name, String teacher, int code, int units,
-                  String days, int hours, int group, int capacity,
-                  ArrayList<Student> regStudents) {
+    public Course(School school, String name, String teacher, int code, int units, int[] days, int[] hours, int group, ArrayList<Student> regStudents, int capacity) {
+        this.school = school;
         this.name = name;
         this.teacher = teacher;
         this.code = code;
@@ -22,7 +36,32 @@ public abstract class Course {
         this.days = days;
         this.hours = hours;
         this.group = group;
+        this.regStudents = regStudents;
         this.capacity = capacity;
+    }
+    //todo: هرکس رجیستر کرد باید یکی به ثبتنامیا اضافه شه و فقط وقتی ولید باشه رجیستر کردن که  کپسیتی منهای ثبتنامیا<0 باشه
+    public String display(){
+        String s = getCode() + " - " +getName()+"\n"+"school: "+ getSchool().name() + "\tteacher: "+ getTeacher()+ "\tgroup: " + getGroup()+
+               "\ncapacity: "+ getCapacity() + "\tregistered: " + getRegStudents().size() + "\tunits: "+ getUnits()+
+                "\ndays: ";
+        String d = "";
+        for (int num: days){
+            d += dayNumbers[num] + " ";
+        }
+        d += "{from: " + startHours[getHours()[0]] +
+                "to: "+ startHours[getHours()[getHours().length-1] + 1] + "}";
+        return s+ d;
+    }
+
+    public School getSchool() {
+        return school;
+    }
+
+    public ArrayList<Student> getRegStudents() {
+        return regStudents;
+    }
+
+    public void setRegStudents(ArrayList<Student> regStudents) {
         this.regStudents = regStudents;
     }
 
@@ -50,11 +89,11 @@ public abstract class Course {
         return units;
     }
 
-    public String getDays() {
+    public int[] getDays() {
         return days;
     }
 
-    public int getHours() {
+    public int[] getHours() {
         return hours;
     }
 

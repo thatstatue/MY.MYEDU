daef3ef3c072766b804f5807b7c2b39f73a92fd0
NoteBook <saradoublemohammadi@gmail.com>
2024-02-23 15:56:26 +0330

    added some courses hardcore + created a property to keep track of all courses

--- src/Database.java
+++ src/Database.java
@@ -3,12 +3,14 @@ import java.util.ArrayList;
 public class Database {
 
     private final ArrayList<User> users;
+    private final ArrayList<Course> courses;
 
     public Database() {
-        this(new ArrayList<>());
+        this(new ArrayList<>(), new ArrayList<>());
     }
-    public Database(ArrayList<User> users){
+    public Database(ArrayList<User> users, ArrayList<Course> courses){
         this.users = users;
+        this.courses = courses;
     }
 
     public void addUser(User user){
@@ -19,12 +21,29 @@ public class Database {
         return users;
     }
 
+    public void addCourse(Course course){
+        courses.add(course);
+    }
+
+    public ArrayList<Course> getCourses() {
+        return courses;
+    }
+
     //todo: only admins can access this
-    public void showStudents(){
+    public void showStudents() {
         System.out.println("Students: ");
-        for (User user : users){
+        for (User user : users) {
             if (user instanceof Student) {
                 System.out.println(user.getUsername());
+            }
+        }
+    }
+
+    public void showCourses(School school){
+        System.out.println("Courses of "+ school.name() + ": ");
+        for (Course course : courses){
+            if (course.getSchool().equals(school)) {
+                System.out.println(course);
             }
         }
     }

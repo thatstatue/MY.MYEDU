1b08faeaac863148c42451fbd8b1ee44d7d02736
NoteBook <saradoublemohammadi@gmail.com>
2024-02-23 15:53:56 +0330

    Police checks if units are less than 21

--- src/Police.java
+++ src/Police.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Police {
+public class Police {
+    public static boolean isValid(Student student, Course course){
+        if (student.getUnits() + course.getUnits() > 20){
+            System.out.println("maximum units per semaster is 20.");
+            return false;
+        }
+        return true;
+    }
+
 }

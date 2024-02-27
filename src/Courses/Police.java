0d60612a6d98ca690f83e0d6444def4331258d96
NoteBook <saradoublemohammadi@gmail.com>
2024-02-28 02:50:07 +0330

    max gen units and max capacity added to police.

--- src/Courses/Police.java
+++ src/Courses/Police.java
@@ -16,27 +16,37 @@ public class Police {
     }
 
     public static boolean isValid(Student student, Course course){
-        if (student.getUnits() + course.getUnits() > 20){
-            System.out.println("WAR: maximum units per semester is 20.");
-            return false;
-        }
-        boolean [][] ws = student.getWeekSchedule();
-        if(!checkBusy(course, ws)){
-            System.out.println("WAR: you have another course in this time period.");
-            return false;
-        }
-        for (Course course1 : student.getRegisteredCourses()){
-            if (course1.getExamDate().equals(course.getExamDate())){
-                if(course1.getExamTime() == course.getExamTime()){
-                    System.out.println("WAR: you have another final exam at this date and time.");
-                    return false;
+        if (student!= null) {
+            if (student.getUnits() + course.getUnits() > 20) {
+                System.out.println("WAR: maximum units amount per semester is 20.");
+                return false;
+            }
+            boolean[][] ws = student.getWeekSchedule();
+            if (!checkBusy(course, ws)) {
+                System.out.println("WAR: you have another course in this time period.");
+                return false;
+            }
+            for (Course course1 : student.getRegisteredCourses()) {
+                if (course1.getExamDate().equals(course.getExamDate())) {
+                    if (course1.getExamTime() == course.getExamTime()) {
+                        System.out.println("WAR: you have another final exam at this date and time.");
+                        return false;
+                    }
                 }
             }
-        }
-        //
-        //WAR: the course capacity is full.
+            if (course.getCapacity() == course.getRegStudents().size()){
+                System.out.println("WAR: the course capacity is full.");
+                return false;
+            }
+            if (course.getUnits() + student.getGenUnits() > 5){
+                System.out.println("WAR: maximum general units number per semester is 5.");
+                return false;
+            }
 
-        return true;
+            return true;
+        }else {
+            return false;
+        }
     }
 
 }
